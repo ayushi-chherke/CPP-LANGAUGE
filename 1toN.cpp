cc3eb32e9b6066b85309e5df641f3fd872b0ecde
ayushi-chherke <140295288+ayushi-chherke@users.noreply.github.com>
2024-08-23 00:28:08 +0530

    Add files via upload

---
+++ 1toN.cpp
@@ -0,0 +1,15 @@
+//Print no from 1 to N
+#include <iostream>
+using namespace std;
+
+int main(){
+int n,i;
+cout<<"Enter no. till you want no. to print ";
+cin>>n;
+while (i<=n)
+{
+  cout<<i<<"\n";
+  i++;
+}
+return 0;
+}
